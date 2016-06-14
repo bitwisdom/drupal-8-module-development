3906baea993da1b16d70c8fea5c3259a71058c99
Wayne Eaker <wayne@zengenuity.com>
2016-06-14 09:04:36 -0400

    HelloUserBlock incorrect XSS handling.

---
+++ block_examples/src/Plugin/Block/HelloUserBlock.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\block_examples\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello User' block.
+ *
+ * @Block(
+ *   id = "block_examples_hello_user",
+ *   admin_label = @Translation("Block Example: Hello User"),
+ *   category = @Translation("Examples")
+ * )
+ */
+class HelloUserBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $user = \Drupal::currentUser();
+    return [
+      '#markup' => $this->t('Hello ' . $user->getDisplayName() . '!!!'),
+    ];
+  }
+
+}
