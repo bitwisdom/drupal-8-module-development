0ed7726f3bf87a59bf655f87d2bae74aaff13bd7
Wayne Eaker <wayne@zengenuity.com>
2016-06-14 12:14:56 -0400

    Configurable ContactInfo Block

--- block_examples/src/Plugin/Block/ContactInfoBlock.php
+++ block_examples/src/Plugin/Block/ContactInfoBlock.php
@@ -15,6 +15,38 @@ use Drupal\Core\Block\BlockBase;
  */
 class ContactInfoBlock extends BlockBase {
 
+  public function defaultConfiguration() {
+    return [
+      'phone' => '',
+      'email' => '',
+    ];
+  }
+  
+  public function blockForm($form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    $form['phone'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Phone Number'),
+      '#required' => TRUE,
+      '#default_value' => $this->configuration['phone'],
+    ];
+    $form['email'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Email'),
+      '#required' => TRUE,
+      '#default_value' => $this->configuration['email'],
+    ];
+    return $form;
+  }
+  
+  public function blockSubmit($form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    if (!$form_state->getErrors()) {
+      $this->configuration['phone'] = $form_state->getValue('phone');
+      $this->configuration['email'] = $form_state->getValue('email');
+    }
+    parent::blockSubmit($form, $form_state);
+  }
+  
+  
   /**
    * {@inheritdoc}
    */
