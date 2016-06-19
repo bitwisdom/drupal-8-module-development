041bc5cebc7e052ac1ca0003d82e16d9cb544ce2
Wayne Eaker <wayne@zengenuity.com>
2016-06-19 10:30:45 -0400

    Added Date Formatter dependency to Holiday Provider service

--- service_examples/src/HolidayProvider.php
+++ service_examples/src/HolidayProvider.php
@@ -6,12 +6,32 @@ use Drupal\service_examples\HolidayProviderInterface;
 
 class HolidayProvider implements HolidayProviderInterface {
   
+  /**
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  private $dateFormatter;
+  
+  function __construct(\Drupal\Core\Datetime\DateFormatterInterface $dateFormatter) {
+    $this->dateFormatter = $dateFormatter;
+  }
+ 
   public function getHolidays() {
-    return [
+    $current_holidays = [];
+    
+    $holiday_patterns = [
       '01-01' => "New Year's Day",
       '07-04' => "Independence Day",
       '12-25' => "Christmas",
     ];
+    $current_year = $this->dateFormatter->format(REQUEST_TIME, 'custom', 'Y');
+    
+    foreach ($holiday_patterns as $date => $holiday) {
+      $current_holiday_date = $current_year . '-' . $date;
+      $formatted_date = $this->dateFormatter->format(strtotime($current_holiday_date));
+      $current_holidays[$formatted_date] = $holiday;
+    }
+    
+    return $current_holidays;
   }
 
 }
