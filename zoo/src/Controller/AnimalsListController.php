9a8d62b413ff9e26c4d42e67b9da575f94a4bff5
Wayne Eaker <wayne@zengenuity.com>
2016-06-21 11:21:13 -0400

    More complex query code

--- zoo/src/Controller/AnimalsListController.php
+++ zoo/src/Controller/AnimalsListController.php
@@ -29,10 +29,15 @@ class AnimalsListController extends ControllerBase {
       $this->t('Type'),
       $this->t('Age'),
       $this->t('Weight'),
+      $this->t('Habitat'),
     ];
     $rows = [];
     
-    $results = $this->connection->query("SELECT * FROM {zoo_animal}");
+    $results = $this->connection->query("SELECT a.*, h.name AS habitat_name 
+      FROM {zoo_animal} a
+      LEFT JOIN {zoo_habitat} h ON a.habitat_id = h.habitat_id 
+      ORDER BY h.name");
+    
     foreach ($results as $record) {
       $age = floor((REQUEST_TIME - $record->birthdate) / (365 * 24 * 3600));
       $rows[] = [
@@ -40,9 +45,10 @@ class AnimalsListController extends ControllerBase {
         $record->type,
         $this->t('@age years', ['@age' => $age]),
         $this->t('@weight kg', ['@weight' => $record->weight]),
+        $record->habitat_name,
       ];
     }
-
+    
     
     return [
       '#theme' => 'table',
@@ -50,4 +56,6 @@ class AnimalsListController extends ControllerBase {
       '#header' => $header,
     ];
   }
+  
+  
 }
