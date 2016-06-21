545d4852d616b7bd9525f85c442be344a9aa6766
Wayne Eaker <wayne@zengenuity.com>
2016-06-21 11:22:49 -0400

    Where clauses with placeholders

--- zoo/src/Controller/AnimalsListController.php
+++ zoo/src/Controller/AnimalsListController.php
@@ -57,5 +57,49 @@ class AnimalsListController extends ControllerBase {
     ];
   }
   
+  public function listAnimalsInHabitat($habitat) {
+    
+    $header = [
+      $this->t('Name'),
+      $this->t('Type'),
+      $this->t('Age'),
+      $this->t('Weight'),
+    ];
+    $rows = [];
+    
+    $results = $this->connection->query("SELECT * FROM {zoo_animal} 
+        WHERE habitat_id = :habitat_id ORDER BY name",
+        [
+          'habitat_id' => $habitat,
+        ]);
+    foreach ($results as $record) {
+      $age = floor((REQUEST_TIME - $record->birthdate) / (365 * 24 * 3600));
+      $rows[] = [
+        $record->name,
+        $record->type,
+        $this->t('@age years', ['@age' => $age]),
+        $this->t('@weight kg', ['@weight' => $record->weight]),
+      ];
+    }
+    
+    return [
+      '#theme' => 'table',
+      '#rows' => $rows,
+      '#header' => $header,
+    ];
+  }
+  
+  public function listAnimalsInHabitatTitle($habitat) {
+    $name = $this->connection->query("SELECT name FROM {zoo_habitat} 
+        WHERE habitat_id = :habitat_id",
+        [
+          'habitat_id' => $habitat,
+        ])->fetchField();
+    if (!empty($name)) {
+      return $this->t('Animals in @habitat', ['@habitat' => $name]);
+    }
+    return $this->t('Habitat Not Found');
+  }
   
 }
+
