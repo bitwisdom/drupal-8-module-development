57e96d89142a17b886d0d76ddcadafbb4457fb18
Wayne Eaker <wayne@zengenuity.com>
2016-06-22 07:46:11 -0400

    Simple Dynamic Query API example

--- zoo/src/Controller/AnimalsListController.php
+++ zoo/src/Controller/AnimalsListController.php
@@ -90,6 +90,50 @@ class AnimalsListController extends ControllerBase {
   }
   
   public function listAnimalsInHabitatTitle($habitat) {
+    $query = $this->connection->select('zoo_habitat', 'h');
+    $query->fields('h', ['name']);
+    $query->condition('h.habitat_id', $habitat, '=');
+    $result = $query->execute();
+    $name = $result->fetchField();
+    if (!empty($name)) {
+      return $this->t('Animals in @habitat', ['@habitat' => $name]);
+    }
+    return $this->t('Habitat Not Found');
+  }
+  
+  public function listAnimalsInHabitatStaticAPI($habitat) {
+    
+    $header = [
+      $this->t('Name'),
+      $this->t('Type'),
+      $this->t('Age'),
+      $this->t('Weight'),
+    ];
+    $rows = [];
+    
+    $results = $this->connection->query("SELECT * FROM {zoo_animal} 
+        WHERE habitat_id = :habitat_id ORDER BY name",
+        [
+          'habitat_id' => $habitat,
+        ]);
+    foreach ($results as $record) {
+      $age = floor((REQUEST_TIME - $record->birthdate) / (365 * 24 * 3600));
+      $rows[] = [
+        $record->name,
+        $record->type,
+        $this->t('@age years', ['@age' => $age]),
+        $this->t('@weight kg', ['@weight' => $record->weight]),
+      ];
+    }
+    
+    return [
+      '#theme' => 'table',
+      '#rows' => $rows,
+      '#header' => $header,
+    ];
+  }
+  
+  public function listAnimalsInHabitatTitleStaticAPI($habitat) {
     $name = $this->connection->query("SELECT name FROM {zoo_habitat} 
         WHERE habitat_id = :habitat_id",
         [
