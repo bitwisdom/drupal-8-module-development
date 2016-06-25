faa2011f0aa23a5d5b36aece4b7c394056646862
Wayne Eaker <wayne@zengenuity.com>
2016-06-25 09:26:27 -0400

    Submission handling added.

--- form_examples/src/Form/ContactForm.php
+++ form_examples/src/Form/ContactForm.php
@@ -55,6 +55,16 @@ class ContactForm extends FormBase {
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
     drupal_set_message($this->t('Thanks for submitting the form!'));
+    
+    $keys = ['name', 'email', 'message'];
+    foreach ($keys as $key) {
+      drupal_set_message($this->t('@key: @value', 
+        [
+          '@key' => $key,
+          '@value' => $form_state->getValue($key),
+        ]
+      ));
+    }
   }
 
 }
