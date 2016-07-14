8c8c9b1c7f2349fc7f7ab2a642030063839d51e5
Wayne Eaker <wayne@zengenuity.com>
2016-07-14 11:01:59 -0400

    Add time to Hello World block

--- block_examples/src/Plugin/Block/HelloWorldBlock.php
+++ block_examples/src/Plugin/Block/HelloWorldBlock.php
@@ -20,7 +20,12 @@ class HelloWorldBlock extends BlockBase {
    */
   public function build() {
     return [
-      '#markup' => $this->t('Hello world!'),
+      '#markup' => $this->t('Hello world! The current time is @time', 
+          [
+            '@time' => \Drupal::service('date.formatter')->format(REQUEST_TIME, 'custom', 'H:i:s')
+          ]
+        ),
+
     ];
   }
 
