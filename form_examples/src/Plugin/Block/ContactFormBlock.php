5fcb68afaa2ca7157a89609955b6224c134e202f
Wayne Eaker <wayne@zengenuity.com>
2019-08-21 08:00:33 -0400

    Add dependency injection example for blocks.

--- form_examples/src/Plugin/Block/ContactFormBlock.php
+++ form_examples/src/Plugin/Block/ContactFormBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\form_examples\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'Contact Form' block.
@@ -13,7 +16,29 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Examples")
  * )
  */
-class ContactFormBlock extends BlockBase {
+class ContactFormBlock extends BlockBase implements ContainerFactoryPluginInterface {
+    
+  /**
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+  
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
   
   /**
    * {@inheritdoc}
